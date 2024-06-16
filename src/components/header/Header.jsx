26bfa8b840604e36cb661786e8504fcc4facc6b6
santoshthapa321 <santosh.thpa.2080@gmail.com>
2024-06-16 07:46:31 +0545

    updated on june 16

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,35 +2,51 @@ import React, { useState } from "react";
 import logo from "../../assets/Untitled-2.png";
 import { IoMenuSharp } from "react-icons/io5";
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import "./Section1.css";
+
+import { WiDayCloudy } from "react-icons/wi";
+import { MdOutlineNightlight } from "react-icons/md";
+//js 
+const navLinks = ["Home", "About", "Skill", "Services", "Contact"];
 const Header = () => {
-  const[closeButtonDekhineCondition, SetButtonDekhineCondition]= useState(false);
-  const handleclick =()=>{
+  const [closeButtonDekhineCondition, SetButtonDekhineCondition] =
+    useState(false);
+  const handleclick = () => {
     SetButtonDekhineCondition(!closeButtonDekhineCondition);
-  
-  }
+  };
   return (
     <>
-    <div className="flex justify-between mt-3 z-20 shadow-slate-700">
-      <div className="flex gap-3 ml-12  items-center ">
-        <img className="h-10 w-10 rounded-full" src={logo} alt="logo" />
-        <a className="font-semibold">Santosh</a>
+      <div className="mainHeader">
+        <div className=" secHeader  flex justify-between mt-3 ">
+          <div className="flex gap-3 ml-12  items-center ">
+            <img className="h-10 w-10 rounded-full" src={logo} alt="logo" />
+            <a className=" logoName font-semibold">Santosh</a>
+          </div>
+          <div className="flex items-center mr-12">
+            <div className=" list-none items-center gap-8 mr-12 md:flex hidden">
+              {navLinks.map((el)=>{
+                return <a>{el}</a>
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="relative">
+          <div className="responsiveBtn absolute top-0.5 md:hidden right-2" onClick={handleclick}>
+            {closeButtonDekhineCondition ? (
+              <IoMdCloseCircleOutline />
+            ) : (
+              <IoMenuSharp />
+            )}
+            {closeButtonDekhineCondition ?<div className="bg-purple-800 px-6 py-6 flex flex-col absolute rounded-lg text-white right-3 top-5">
+              {navLinks.map((el)=>{
+                return <div>{el}</div>
+              })}
+            </div> :null }
+            
+          </div>
+        </div>
       </div>
-      <div className=" list-none items-center gap-8 mr-12 md:flex hidden">
-        <a href=""> Home</a>
-
-        <a href="aboutpage">About</a>
-
-        <a href="">Skill</a>    
-        <a href="">Services</a>
-        <a href="">Contact</a>
-      </div>
-      <div className="sm:hidden mr-12" onClick={handleclick}>{closeButtonDekhineCondition ?  <IoMdCloseCircleOutline />:<IoMenuSharp /> }
-    
-      </div>
-    </div>
-   
     </>
-    
   );
 };
 
--- src/components/section 1/Section1.jsx
+++ src/components/section 1/Section1.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import imagee from "../../assets/featured1.jpg";
+import imagee from "../../assets/Untitled-2.png";
 import { IoMail } from "react-icons/io5";
 import { MdCloudDownload } from "react-icons/md";
 import { CiLinkedin } from "react-icons/ci";
 import { CiFacebook } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import { IoLogoInstagram } from "react-icons/io";
+import "../header/Section1.css";
 const Section1 = () => {
   return (
-    <div className="flex m-12 justify-between ">
+    <div className="container flex mx-2 mt-12 justify-between ">
       <div className="w-1/2">
-        <div className="">
-          <h1 className="font-bold text-4xl ">
+        <div className="headingCustom">
+          <h1 className=" head1 font-bold text-4xl ">
             I'm <span>Santosh Thapa</span>
           </h1>
 
@@ -19,7 +20,7 @@ const Section1 = () => {
             a <span className="text-purple-800">Full Stack Developer</span>
           </p>
         </div>
-        <div className=" flex gap-8 mt-8">
+        <div className=" primaryBtn flex gap-8 mt-8">
           <a>
             <button
               type="button"
@@ -43,13 +44,13 @@ const Section1 = () => {
         </div>
         <div className="mt-8">
           <p className="">
-            <i></i>I can help you to build attractive and fully functional web
+            I can help you to build attractive and fully functional web
             application for your projects. So let's Talk!
           </p>
         </div>
-        <div className="flex mt-12 items-center">
+        <div className=" followOption flex mt-12 gap-20 items-center">
           <p>Follow me : </p>
-          <div className="flex gap-8 ml-6">
+          <div className="flex gap-8 ">
             <a href="" className=" border-gray-950 border-2 rounded-full p-2">
               <i>
                 <CiLinkedin />
@@ -73,8 +74,8 @@ const Section1 = () => {
           </div>
         </div>
       </div>
-      <div className=" flex w-1/3">
-        <img className="rounded-3xl h-40 " src={imagee} alt="featuredImg1" />
+      <div className="forAm flex justify-center items-center  w-1/3">
+        <img className=" rounded-full  " src={imagee} alt="featuredImg1" />
       </div>
     </div>
   );
